da24aa991b6eee7cb661a5170a7411d2a8c5cba4
w-jinkui <1476969724@qq.com>
2018-07-02 22:13:57 +0800

    Add files via upload

---
+++ testFuse.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 25 16:51:45 2018
+
+@author: wangjinkui
+"""
+import tensorflow as tf
+import numpy as np
+from tensorflow.contrib import rnn
+import pandas as pd
+
+TestLog1 = pd.read_csv('H:\\fire helmet\\TestLog1.csv')
+TestLog2 = pd.read_csv('H:\\fire helmet\\TestLog2.csv')
+Log1 = TestLog1.values
+Log2 = TestLog2.values
+train_Xdata = Log1[0:20000,1:7]
+train_Ydata = Log1[0:2000,10:13]
+test_Xdata  = Log2[0:19000,1:7]
+test_Ydata  = Log2[0:1900,10:13]
+
+class Config(object):
+    """
+    define a class to store parameters,
+    the input should be feature mat of training and testing
+    Note: it would be more interesting to use a HyperOpt search space:
+    https://github.com/hyperopt/hyperopt
+    """
+    def __init__(self, X_train, X_test):
+        # Input data
+        self.train_count = len(X_train)  # 7352 training series
+        self.test_data_count = len(X_test)  # 2947 testing series
+        self.n_steps = 10  # 128 time_steps per series
+
+        # Training
+        self.learning_rate = 0.0025
+        self.lambda_loss_amount = 0.0015
+        self.training_epochs = 300
+        self.batch_size = 10
+
+        # LSTM structure
+        self.n_inputs = 6  # Features count is of 9: 3 * 3D sensors features over time
+        self.n_hidden = 128  # nb of neurons inside the neural network
+        self.n_classes = 3  # Final output classes
+        self.W = {
+            'hidden': tf.Variable(tf.random_normal([self.n_inputs, self.n_hidden])),
+            'output': tf.Variable(tf.random_normal([self.n_hidden, self.n_classes]))
+        }
+        self.biases = {
+            'hidden': tf.Variable(tf.random_normal([self.n_hidden], mean=1.0)),
+            'output': tf.Variable(tf.random_normal([self.n_classes]))
+        }
+
+
+def LSTM_Network(_X, config):
+    """Function returns a TensorFlow RNN with two stacked LSTM cells
+    Two LSTM cells are stacked which adds deepness to the neural network.
+    Note, some code of this notebook is inspired from an slightly different
+    RNN architecture used on another dataset, some of the credits goes to
+    "aymericdamien".
+    Args:
+        _X:     ndarray feature matrix, shape: [batch_size, time_steps, n_inputs]
+        config: Config for the neural network.
+    Returns:
+        This is a description of what is returned.
+    Raises:
+        KeyError: Raises an exception.
+      Args:
+        feature_mat: ndarray fature matrix, shape=[batch_size,time_steps,n_inputs]
+        config: class containing config of network
+      return:
+              : matrix  output shape [batch_size,n_classes]
+    """
+    # (NOTE: This step could be greatly optimised by shaping the dataset once
+    # input shape: (batch_size, n_steps, n_input)
+    _X = tf.transpose(_X, [1, 0, 2])  # permute n_steps and batch_size
+    # Reshape to prepare input to hidden activation
+    _X = tf.reshape(_X, [-1, config.n_inputs])
+    # new shape: (n_steps*batch_size, n_input)
+
+    # Linear activation
+    _X = tf.nn.relu(tf.matmul(_X, config.W['hidden']) + config.biases['hidden'])
+    # Split data because rnn cell needs a list of inputs for the RNN inner loop
+    _X = tf.split(_X, config.n_steps, 0)    
+    # new shape: n_steps * (batch_size, n_hidden)
+
+    # Define two stacked LSTM cells (two recurrent layers deep) with tensorflow
+    lstm_cell_1 = tf.contrib.rnn.BasicLSTMCell(config.n_hidden, forget_bias=1.0, state_is_tuple=True)
+    lstm_cell_2 = tf.contrib.rnn.BasicLSTMCell(config.n_hidden, forget_bias=1.0, state_is_tuple=True)
+    lstm_cells  = tf.contrib.rnn.MultiRNNCell([lstm_cell_1, lstm_cell_2], state_is_tuple=True)
+    # Get LSTM cell output
+    outputs, states = tf.contrib.rnn.static_rnn(lstm_cells, _X, dtype=tf.float32)
+
+    # Get last time step's output feature for a "many to one" style classifier,
+    # as in the image describing RNNs at the top of this page
+    lstm_last_output = outputs[-1]
+
+    # Linear activation
+    return tf.matmul(lstm_last_output, config.W['output']) + config.biases['output']
+
+config = Config(train_Xdata, test_Xdata)
+#train_Xdata = tf.reshape(train_Xdata_raw,[-1,config.n_steps,config.n_inputs])
+#test_Xdata  = tf.reshape(test_Xdata_raw,[-1,config.n_steps,config.n_inputs])
+X = tf.placeholder(tf.float32, [None, config.n_steps, config.n_inputs])
+Y = tf.placeholder(tf.float32, [None, config.n_classes])
+train_Xdata = np.reshape(train_Xdata, [-1, config.n_steps,config.n_inputs])
+test_Xdata = np.reshape(test_Xdata, [-1, config.n_steps,config.n_inputs])
+pred_Y = LSTM_Network(X, config)
+    # Loss,optimizer,evaluation
+l2 = config.lambda_loss_amount * \
+        sum(tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables())
+    # Softmax loss and L2
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=pred_Y)) + l2
+optimizer = tf.train.AdamOptimizer(learning_rate=config.learning_rate).minimize(cost)
+correct_pred = tf.equal(tf.argmax(pred_Y, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, dtype=tf.float32))
+    
+sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=False))
+init = tf.global_variables_initializer()
+sess.run(init)
+best_accuracy = 0.0
+    # Start training for each batch and loop epochs
+for i in range(config.training_epochs):
+        for start, end in zip(range(0, config.train_count, config.batch_size),range(config.batch_size, config.train_count + 1, config.batch_size)):
+            sess.run(optimizer, feed_dict={X: train_Xdata[start:end], Y: train_Ydata[start:end]})
+        # Test completely at every epoch: calculate accuracy
+        pred_out, accuracy_out, loss_out = sess.run([pred_Y, accuracy, cost],feed_dict={X: test_Xdata,Y: test_Ydata})
+        print("traing iter: {},".format(i) + " test accuracy : {},".format(accuracy_out) + " loss : {}".format(loss_out))
+        best_accuracy = max(best_accuracy, accuracy_out)
+print("")
+print("final test accuracy: {}".format(accuracy_out))
+print("best epoch's test accuracy: {}".format(best_accuracy))
+print("")  
+
+    
+    
